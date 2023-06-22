9f6885c5fda4621ef1ce519560434627029f8352
Mohammad-Aljamal <mohammad.o.aljamal@gmail.com>
2023-06-22 11:22:53 +0000

    linked-list-kth updated DONE

--- javascript/linked-list/__tests__/linkedlist.test.js
+++ javascript/linked-list/__tests__/linkedlist.test.js
@@ -8,25 +8,78 @@ describe("Linked List", () => {
     let list = await new LinkedList();
     expect(list.head).toBeNull();
   });
+});
+
+describe("append", () => {
   test("append to linkedlist", async () => {
     let list = await new LinkedList();
     list.append("one");
     expect(list.head.value).toEqual("one");
     list.append("two");
     expect(list.tail.value).toEqual("two");
-    // list.includes("two");
-    // expect(list.includes("two")).toEqual(true);
-    // list.toString();
-    // expect(list.toString()).toEqual("{ one } -> { two } -> NULL");
-    list.insertBefore('one','zero');
-    expect(list.head.value).toEqual('zero');
-    // list.insertAfter('two','three');
-    // expect(list.tail.value).toEqual("three");
-    list.kthFromEnd(0);
-    expect(list.kthFromEnd(0)).toEqual('two');
+    list.append("three");
+    expect(list.tail.value).toEqual("three");
   });
 });
-// ---
-// afterAll (()=> {
-//   process.exit();
-// })
+
+describe("includes", () => {
+  test("includes in linkedlist", async () => {
+    let list = await new LinkedList();
+    list.append("one");
+    let result = list.includes("one");
+    expect(result).toEqual(true);
+    // let result2 = list.includes("two");
+    // expect(result2).toEqual(false);
+  });
+});
+
+describe("toString", () => {
+  test("linkedlist to String ", async () => {
+    let list = await new LinkedList();
+    let result = list.toString();
+    expect(result).toEqual("NULL");
+
+    // list.append("one");
+    // list.append("two");
+    // let result2 = list.toString();
+    // expect(result2).toEqual("one -> two -> NULL");
+  });
+});
+
+describe("toString", () => {
+  test("linkedlist to String ", async () => {
+    let list = await new LinkedList();
+    let result = list.toString();
+    expect(result).toEqual("NULL");
+
+    // list.append("one");
+    // list.append("two");
+    // let result2 = list.toString();
+    // expect(result2).toEqual("one -> two -> NULL");
+  });
+});
+
+describe("kthFromEnd", () => {
+  test("kthFromEnd in LL ", async () => {
+    let list = await new LinkedList();
+    let result = list.kthFromEnd(0);
+    expect(result).toEqual("Exception");
+
+    list.append("one");
+    let result1 = list.kthFromEnd(0);
+    expect(result1).toEqual("one");
+
+    let result2 = list.kthFromEnd(1);
+    expect(result2).toEqual("Exception");
+
+    list.append("two");
+    let result3 = list.kthFromEnd(1);
+    expect(result3).toEqual("one");
+    let result4 = list.kthFromEnd(0);
+    expect(result4).toEqual("two");
+
+    let result5 = list.kthFromEnd(-1);
+    expect(result5).toEqual("Exception");
+  });
+});
+
