8ce66fd6b1944b2fd53138c73c66bb0d59f119e3
Mohammad-Aljamal <mohammad.o.aljamal@gmail.com>
2023-07-04 19:07:19 +0000

    test

---
+++ javascript/stack-and-queue/stack-queue-pseudo/stack-queue-pseudo.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const Node = require('../Node');
+
+class Queue {
+    constructor (){
+        this.front = null;
+        this.rear = null;
+        this.length = 0;
+    }
+
+    isEmpty(){
+        if(this.front === null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    enqueue(value){
+        const newNode = new Node (value);
+
+        if(this.isEmpty()){
+            this.front = newNode;
+            this.rear = newNode;
+            this.length++;
+            return newNode;
+        }else{
+            newNode.next = this.front;
+            this.front = newNode;
+            this.length++;
+            return newNode;
+        }
+    }
+
+    dequeue(){
+        if(this.isEmpty()){
+            console.log('the queue is empty')
+            return 'the queue is empty';
+        }else{
+            const temp = this.rear;
+            this.rear = temp.next;
+            temp.next = null;
+            this.length--;
+            return temp.value;
+            // const current 
+            // while()
+        }
+    }
+
+    peek(){
+        if(this.isEmpty() === true){
+            console.log('the queue is empty')
+            return 'the queue is empty';
+        }else{
+            return this.front.value;
+        }
+    }
+}
+
+module.exports = Queue;
