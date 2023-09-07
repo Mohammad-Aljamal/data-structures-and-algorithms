455c1208c25d8db0c66e0167f2438d3150bbdb97
Mohammad-Aljamal <mohammad.o.aljamal@gmail.com>
2023-09-07 03:20:47 +0000

    2/3 done

---
+++ javascript/graph/graph.js
@@ -0,0 +1,80 @@
+"use strict"
+
+const Edge = require("./edge");
+const Vertex = require("./vertex");
+
+class Graph {
+    constructor() {
+        this.adjacencyList = new Map();
+    }
+    addVertex(vertex) {
+        this.adjacencyList.set(vertex, []);
+    }
+    addDirectedEdge(start, end, weight) {
+        if (!this.adjacencyList.has(start) || !this.adjacencyList.has(end)) {
+            console.log(`vertex does not exist`)
+            return;
+        }
+        else {
+            const adjacencies = this.adjacencyList.get(start);
+            let edge = new Edge(end, weight);
+            adjacencies.push(edge);
+        }
+    }
+    getvertices () {
+        return this.adjacencyList.keys();
+    }
+    getneighbors (vertex) {
+        // const iterator1 = this.adjacencyList.keys()
+        // for(let i =0; i>0; i++ ){
+        //     const s = iterator1.next().value
+        //     if(s == undefined){
+        //         i = 0;
+        //         break;
+        //     }
+        //     console.log(s);
+
+        // }
+        // return this.adjacencyList.get(vertex)
+    }
+    size () {
+        return this.adjacencyList.size;
+
+    }
+}
+
+const zero = new Vertex(0);
+const one = new Vertex(1);
+const two = new Vertex(2);
+const three = new Vertex(3);
+const four = new Vertex(4);
+const five = new Vertex(5);
+
+const myGraph = new Graph();
+
+myGraph.addVertex(zero);
+myGraph.addVertex(one);
+myGraph.addVertex(two);
+myGraph.addVertex(three);
+myGraph.addVertex(four);
+myGraph.addVertex(five);
+
+myGraph.addDirectedEdge(zero, five);
+myGraph.addDirectedEdge(zero, three);
+myGraph.addDirectedEdge(three, one);
+myGraph.addDirectedEdge(four, one);
+myGraph.addDirectedEdge(two, three);
+myGraph.addDirectedEdge(zero, two);
+myGraph.addDirectedEdge(five, four);
+
+const mynewGraph = new Graph();
+
+
+console.log(myGraph.getvertices());
+
+console.log(mynewGraph.getvertices());
+
+// myGraph.getneighbors(one)
+
+console.log(myGraph.size());
+
